9b00ff1e8e1da12deff2c10d1caeff5211050acf
Claudia Saxer <c.s.saxer@gmail.com>
2016-09-29 16:13:43 +0200

    add idbcommit

--- .src/Intranet/Definitions/Dal/IDbCommit.cs
+++ .src/Intranet/Definitions/Dal/IDbCommit.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Intranet.Definition.Dal
 {
-    class IDbCommit
+    /// <summary>
+    ///     Interface representing a class able to commit changes to the database.
+    /// </summary>
+    public interface IDbCommit<TContext> : IDisposable
     {
+        /// <summary>
+        ///     Commits all pending changes to the database.
+        /// </summary>
+        /// <returns>Returns the number of changed records.</returns>
+        Int32 Commit();
+
+        /// <summary>
+        ///     Commits all pending changes to the database asynchronously.
+        /// </summary>
+        /// <returns>Returns the number of changed records asynchronously.</returns>
+        Task<Int32> CommitAsync(CancellationToken? cancellationToken = null);
     }
 }
