ba1d07dbb929e205f89a51d84f192a0844d195e8
Claudia Saxer <c.s.saxer@gmail.com>
2016-09-29 17:06:53 +0200

    add migrations add context

---
+++ .src/Intranet/Dal/IntranetContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intranet.Model;
+
+namespace Intranet.Dal
+{
+    public class IntranetContext : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+    }
+
+}
