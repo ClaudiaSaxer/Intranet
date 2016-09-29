814529754cf7573279303f18693743593e0a8590
Claudia Saxer <c.s.saxer@gmail.com>
2016-09-29 16:12:54 +0200

    idatabasefactory

---
+++ .src/Intranet/Definitions/Dal/IDatabaseFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intranet.Definition.Dal
+{
+    /// <summary>
+    ///     Interface representing a class creating database contexts.
+    /// </summary>
+    public interface IDatabaseFactory<out TContext> : IDisposable
+        where TContext : DbContext, new()
+    {
+        /// <summary>
+        ///     Gets a database context.
+        /// </summary>
+        /// <returns>Returns a database context.</returns>
+        TContext GetDb();
+    }
+}
