183d782e8d8d76bf990c6f06a0d5964a9845072b
Claudia Saxer <c.s.saxer@gmail.com>
2016-10-04 21:07:15 +0200

    Bla

--- .src/Intranet.Testing/Bll/TestTestUseAutofac.cs
+++ .src/Intranet.Testing/Bll/TestTestUseAutofac.cs
@@ -6,27 +6,26 @@ namespace Intranet.Bll.Test
 {
     public class TestTestUseAutofac
     {
-       
-
         [Fact]
         public void HelloWorldAndNumberTest()
         {
-
-            using (var mock = AutoMock.GetLoose())
+            using ( var mock = AutoMock.GetLoose() )
             {
                 // Arrange - configure the mock
-                mock.Mock<ITestAutofac>().Setup(x => x.GetHelloWorld("world")).Returns("Hello world");
+                mock.Mock<ITestAutofac>()
+                    .Setup( x => x.GetHelloWorld( "world" ) )
+                    .Returns( "Hello world" );
                 var sut = mock.Create<ITestAutofac>();
                 var target = new TestUseAutofac { TestAutofac = sut };
                 // Act
 
-                var hello = target.GetHelloWorldAndNumer("world", 42);
+                var hello = target.GetHelloWorldAndNumer( "world", 42 );
 
                 // Assert - assert on the mock
-                mock.Mock<ITestAutofac>().Verify(x => x.GetHelloWorld("world"));
-                Assert.Equal("Hello world 42", hello);
+                mock.Mock<ITestAutofac>()
+                    .Verify( x => x.GetHelloWorld( "world" ) );
+                Assert.Equal( "Hello world 42", hello );
             }
-
         }
     }
 }
