5a135e094035cb95910bfeec7467b6fea05f0b9f
Claudia Saxer <c.s.saxer@gmail.com>
2016-10-10 15:46:48 +0200

    modules

--- .src/Intranet/Model/Module.cs
+++ .src/Intranet/Model/Module.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿#region Usings
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#endregion
 
 namespace Intranet.Model
 {
-    class Module
+    /// <summary>
+    ///     Module that will be in the IntranetShell
+    /// </summary>
+    public class Module
     {
+        #region Properties
+
+        [Key]
+        [DatabaseGenerated( DatabaseGeneratedOption.Identity )]
+        [Column( "Id" )]
+        public Guid ModuleId { get; set; }
+
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Path { get; set; }
+
+
+        /// <summary>
+        /// Collection of Submodules to this Module
+        /// </summary>
+        public virtual ICollection<Submodule> Submodules { get; set; }
+
+        #endregion
     }
 }
--- .src/Intranet/Model/Submodule.cs
+++ .src/Intranet/Model/Submodule.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Intranet.Model
 {
-    class Submodule
+    public class Submodule
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Id")]
+        public Guid SubmoduleId { get; set; }
+
+        public String Name { get; set; }
+        public String Description { get; set; }
     }
 }
