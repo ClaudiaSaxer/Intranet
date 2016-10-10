7a0420105ae111183ed7a650caaacb72f6e75b7b
Claudia Saxer <c.s.saxer@gmail.com>
2016-10-10 13:39:49 +0200

    sass task

--- .src/Intranet/Web/gulpfile.js
+++ .src/Intranet/Web/gulpfile.js
@@ -1,10 +1,14 @@
-﻿/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
+﻿'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
