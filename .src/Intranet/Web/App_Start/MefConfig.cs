3abc66bd3504d677303d375a779a84a270e15708
Claudia Saxer <c.s.saxer@gmail.com>
2016-10-12 08:44:21 +0200

    coment mef configuration and make static

--- .src/Intranet/Web/App_Start/MefConfig.cs
+++ .src/Intranet/Web/App_Start/MefConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 using Intranet.Web.IoC;
@@ -7,12 +8,15 @@ using Intranet.Web.IoC;
 namespace Intranet.Web.App_Start
 {
     /// <summary>
+    /// Class representing the configuration for Mef
     /// </summary>
-    public class MefConfig
+    public static class MefConfig
     {
         /// <summary>
+        /// Configurate the container
         /// </summary>
-        public static void ConfigureContainer(List<String> pluginPaths) 
+        /// <param name="pluginPaths">String with pathes for the Plugins</param>
+        public static void ConfigureContainer(IEnumerable<String> pluginPaths) 
             => Bootstrapper.Compose( pluginPaths );
     }
 }
