f7aeaaabe33a07023ef86753fc16d42ce6653019
Claudia Saxer <c.s.saxer@gmail.com>
2016-10-12 08:44:57 +0200

    cleanup

--- .src/Intranet/Web/App_Start/MefConfig.cs
+++ .src/Intranet/Web/App_Start/MefConfig.cs
@@ -1,22 +1,23 @@
-﻿using System;
+﻿#region Usings
+
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Web;
 using Intranet.Web.IoC;
+
+#endregion
 
 namespace Intranet.Web.App_Start
 {
     /// <summary>
-    /// Class representing the configuration for Mef
+    ///     Class representing the configuration for Mef
     /// </summary>
     public static class MefConfig
     {
         /// <summary>
-        /// Configurate the container
+        ///     Configurate the container
         /// </summary>
         /// <param name="pluginPaths">String with pathes for the Plugins</param>
-        public static void ConfigureContainer(IEnumerable<String> pluginPaths) 
+        public static void ConfigureContainer( IEnumerable<String> pluginPaths )
             => Bootstrapper.Compose( pluginPaths );
     }
 }
