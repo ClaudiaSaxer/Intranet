80f07d878ac69a1c4add9bd4a114ae65aabc445b
Frank Jordi <frank-jordi@hotmail.com>
2016-10-17 10:44:26 +0200

    add HomeViewModel

---
+++ .src/Intranet/ViewModel/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intranet.Model;
+
+namespace Intranet.ViewModel
+{
+    /// <summary>
+    ///     Class representing the ViewModel for the HomeController
+    /// </summary>
+    public class HomeViewModel
+    {
+        /// <summary>
+        ///     Gets or sets the Modules for the HomeViewModel
+        /// </summary>
+        /// <value>
+        ///     The Modules
+        /// </value>
+        public IEnumerable<Module> Modules { get; set; }
+    }
+}
