143b5637d043bc8842b0e281d964d12ba9fff28c
Frank Jordi <frank-jordi@hotmail.com>
2016-10-17 11:45:02 +0200

    add missings lines cause of not saving the solution

--- .src/Intranet/Definitions/Bll/ISettingsService.cs
+++ .src/Intranet/Definitions/Bll/ISettingsService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Intranet.ViewModel;
 
 namespace Intranet.Definition.Bll
 {
-    interface ISettingsService
+    /// <summary>
+    ///     The Interface for the SettingsController
+    /// </summary>
+    public interface ISettingsService
     {
+        /// <summary>
+        ///     Gets the SettingsViewModel
+        /// </summary>
+        /// <returns>The SettingsViewModel</returns>
+        SettingsViewModel GetSettingsViewModel();
+
+        /// <summary>
+        ///     Updates the settings for the module
+        /// </summary>
+        /// <param name="moduleSetting">The ModuleSettings</param>
+        void UpdateModuleSetting(ModuleSetting moduleSetting);
     }
 }
