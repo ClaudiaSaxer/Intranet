2a189102c95d0d26e6ca356525484abf650a6289
Frank Jordi <frank-jordi@hotmail.com>
2016-10-17 11:19:15 +0200

    add SettingsViewModel

---
+++ .src/Intranet/ViewModel/ModuleSetting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intranet.ViewModel
+{
+    /// <summary>
+    ///     Class representing ModuleSettings
+    /// </summary>
+    public abstract class ModuleSetting
+    {
+        /// <summary>
+        ///     Gets or sets the name of the module
+        /// </summary>
+        /// <value>The name of the module</value>
+        public String Name { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the visibility of the module
+        /// </summary>
+        /// <value>The visibility status of the module</value>
+        public Boolean Visible { get; set; }
+    }
+}
---
+++ .src/Intranet/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intranet.ViewModel
+{
+    /// <summary>
+    ///     Class representing a SettingsViewModel
+    /// </summary>
+    public class SettingsViewModel
+    {
+        /// <summary>
+        ///     Gets or sets the ModuleSettings for the SettingsViewModel
+        /// </summary>
+        /// <value>
+        ///     The ModuleSettings
+        /// </value>
+        public IEnumerable<ModuleSetting> ModuleSettings { get; set; }
+    }
+}
