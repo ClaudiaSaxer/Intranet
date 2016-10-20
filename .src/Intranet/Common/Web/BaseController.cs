3a6e8231e59fc754ef4febbfbfe3ecfc3f65aa9c
Frank Jordi <frank-jordi@hotmail.com>
2016-10-20 14:46:28 +0200

    folders

---
+++ .src/Intranet/Common/Web/BaseController.cs
@@ -0,0 +1,54 @@
+﻿using System.Web.Mvc;
+
+namespace Intranet.Common
+{
+    /// <summary>
+    ///     Abstract base class for MVC controllers.
+    /// </summary>
+    public abstract class ControllerBase : Controller
+    {
+        #region Fields
+
+        /// <summary>
+        ///     The logger used by the controller.
+        /// </summary>
+        protected readonly ILogger Logger;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the viewmodel for the navigation
+        /// </summary>
+        public INavigationService NavigationService { get; set; }
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        ///     Initialize a new instance of the <see cref="ControllerBase" /> class.
+        /// </summary>
+        /// <param name="logger">A <see cref="ILogger" />.</param>
+        protected ControllerBase( ILogger logger )
+        {
+            Logger = logger;
+            Logger.Trace( "Enter Ctor - Exit." );
+        }
+
+        #endregion
+
+        // GET: Navigation
+        /// <summary>
+        ///     The Partial View Result for the Navigation with the MainModules
+        /// </summary>
+        /// <returns>Partial View Result with Navigation Modules</returns>
+        [AllowAnonymous]
+        [ChildActionOnly]
+        public ActionResult GetNavigation()
+        {
+            return PartialView( "_HeaderNavBar", NavigationService.GetNavigationViewModel() );
+        }
+    }
+}
---
+++ .src/Intranet/Web/assets/gulpfile.js
@@ -0,0 +1,93 @@
+﻿'use strict';
+
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var ts = require('gulp-typescript');
+var rimraf = require('rimraf');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
+var minify = require('gulp-minify');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+
+var tsPath = './ts';
+var tsconfigPath = tsPath + '/tsconfig.json';
+var sassPath = './sass';
+var jsPath = '../resources/js/intigena';
+var cssPath = '../resources/css/intigena';
+
+// Delete the dist directory
+gulp.task('clean',
+    function() {
+        return gulp.src([jsPath,cssPath])
+            .pipe(clean({ force: true }));
+    });
+
+
+var tsProject = ts.createProject(tsconfigPath,
+{
+    typescript: require('typescript')
+});
+
+gulp.task('ts',
+    function() {
+
+        var tsResult = gulp.src(tsPath+'/*.ts')
+            .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
+        return tsResult.js.pipe(gulp.dest(jsPath));
+    });
+
+gulp.task('sass',
+    function() {
+        return gulp.src([sassPath+'/**/*.scss', '!'+sassPath+'/bootstrap'])
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest(cssPath));
+    });
+
+
+gulp.task('minify-js',
+    ['ts'],
+    function() {
+        gulp.src([jsPath+'/**/*.js', '!'+jsPath+'**/*min.js'])
+            .pipe(minify({
+                ext: {
+                    src: '.js',
+                    min: '.min.js'
+                },
+                exclude: ['tasks'],
+                ignoreFiles: ['.combo.js', '.min.js']
+            }))
+            .pipe(gulp.dest(jsPath));
+    });
+
+gulp.task('minify-css',
+    ['sass'],
+    function() {
+        return gulp.src([
+                cssPath+'/*.css', '!'+cssPath+'/*.min.css'
+            ])
+            .pipe(cleanCSS({ compatibility: 'ie8' }))
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest(cssPath));
+    });
+
+gulp.task('watch.sass',
+    ['sass'],
+    function() {
+        gulp.watch(sassPath+'/**.scss', ['sass']);
+    });
+gulp.task('watch.ts',
+    ['ts'],
+    function() {
+        return gulp.watch(tsPath+'/*.ts', ['ts']);
+    });
+gulp.task('watch', ['watch.ts']);
+gulp.task('watch', ['watch.sass']);
+
+gulp.task('default', ['ts', 'sass', 'minify-js', 'minify-css']);
