3b91a52b9d25d0303cf5c534bf77734e0c2b70aa
Frank Jordi <frank-jordi@hotmail.com>
2016-10-24 10:52:43 +0200

    remove unused using

--- .src/Intranet.Testing/TestEnvironment/MockHelperService.cs
+++ .src/Intranet.Testing/TestEnvironment/MockHelperService.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Intranet.Definition;
+﻿using Intranet.Definition;
+using Intranet.Definition.Bll;
 using Intranet.ViewModel;
 using Moq;
 
