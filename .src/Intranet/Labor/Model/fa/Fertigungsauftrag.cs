975728e0019a3a832fce944fa1846c25d4fba319
Saxer Claudia <csaxer@hsr.ch>
2016-10-27 13:33:24 +0200

    fertigungsauftrag

---
+++ .src/Intranet/Labor/Model/fa/Fertigungsauftrag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Intranet.Labor.Model.fa
+{
+    /// <summary>
+    ///     The Class representing the Fertigungsauftrag
+    /// </summary>
+    public class Fertigungsauftrag
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the id of the Fertigungsauftrag
+        /// </summary>
+        /// <value>the id of the Fertigungsauftrag</value>
+        public Int32 FaId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the number of the Fertigungsauftrag
+        /// </summary>
+        /// <value>the number of the Fertigungsauftrag</value>
+        public String FaNr { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the start Date and Time of the Fertigungsauftrag
+        /// </summary>
+        /// <value>The start date and time of the Fertigungsauftrag</value>
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the end Data and Time of the Fertigungsauftrag
+        /// </summary>
+        /// <value>The end date and time of the Fertigungsauftrag</value>
+        public DateTime End { get; set; }
+
+        #endregion
+    }
+}
