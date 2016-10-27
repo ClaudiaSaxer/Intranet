3840eecadc16b9375c689ad74bc20d3dc0b9bffd
Saxer Claudia <csaxer@hsr.ch>
2016-10-27 13:29:45 +0200

    instron db

---
+++ .src/Intranet/Labor/Model/UniqueKeyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intranet.Labor
+{
+    /// <summary>
+    ///     Class representing a unique attribute.
+    /// </summary>
+    [AttributeUsage( AttributeTargets.Property )]
+    public class UniqueKeyAttribute : Attribute
+    {
+    }
+}
---
+++ .src/Intranet/Labor/Model/instron/Instron.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intranet.Labor.Model
+{
+    /// <summary>
+    ///     Class representing Instron
+    /// </summary>
+    public class Instron
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the Id of Instron
+        /// </summary>
+        /// <value>The Id for Instron</value>
+        [Key]
+        [DatabaseGenerated( DatabaseGeneratedOption.Identity )]
+        private Int32 IntstronId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Fertigungsauftrag Number
+        /// </summary>
+        /// <value>The Fertigungsauftrag Number of Instron.</value>
+        [UniqueKey]
+        private String FaNr { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the DateTime from the Instron Test
+        /// </summary>
+        /// <value>The Date and Time from the Test</value>
+        private DateTime TestDateTime { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a Collection from Instron Values <see cref="InstronValues" />
+        /// </summary>
+        /// <value>The Values for the Instron</value>
+        public ICollection<InstronValue> InstronValues { get; set; }
+
+        #endregion
+    }
+}
