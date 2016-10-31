0c6c0fd75bd50ef90592ccc04167376baaff30e7
Frank Jordi <frank-jordi@hotmail.com>
2016-10-31 11:16:03 +0100

    create mock for LaborHomeBll

---
+++ .src/Intranet.Testing/Labor/TestEnvironment/MockHelperBll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intranet.Labor.Definition.Bll;
+using Intranet.Model;
+using Moq;
+
+namespace Intranet.Labor.TestEnvironment
+{
+    /// <summary>
+    ///     A mock helper class for bll
+    /// </summary>
+    public static class MockHelperBll
+    {
+        /// <summary>
+        ///     A mock for LaborHomeBll
+        /// </summary>
+        /// <param name="modules">Modules returned by AllLaborModulesForRoles</param>
+        /// <returns></returns>
+        public static ILaborHomeBll GetLaborHomeBll(IEnumerable<Module> modules)
+        {
+            var mock = new Mock<ILaborHomeBll>
+            {
+                Name = "MockHelper.GetLaborHomeBll",
+                DefaultValue = DefaultValue.Mock
+            };
+
+            mock.Setup(x => x.AllLaborModulesForRoles(It.IsAny<IEnumerable<String>>()))
+                .Returns(modules);
+
+            return mock.Object;
+        }
+    }
+}
