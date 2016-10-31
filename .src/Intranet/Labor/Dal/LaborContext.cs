ae6bdbccc1eb73768f8115adbefc2d03d06915de
Saxer Claudia <csaxer@hsr.ch>
2016-10-31 10:02:36 +0100

    cleanup

--- .src/Intranet/Labor/Dal/LaborContext.cs
+++ .src/Intranet/Labor/Dal/LaborContext.cs
@@ -20,7 +20,7 @@ namespace Intranet.Labor.Dal
         ///     Gets or sets the components for the labor
         /// </summary>
         /// <value>the components</value>
-        public DbSet<ProductionOrderComponent> Components { get; set; }
+        public DbSet<ProductionOrderComponent> ProductionOrderComponent { get; set; }
 
         /// <summary>
         ///     Gets or sets the errors for the labor
@@ -67,24 +67,6 @@ namespace Intranet.Labor.Dal
         /// <param name="modelBuilder"> The builder that defines the model for the context being created. </param>
         protected override void OnModelCreating( DbModelBuilder modelBuilder )
         {
-           /* //ProductionOrder - Component
-            modelBuilder.Entity<Component>()
-                        .HasRequired( x => x.ProductionOrder )
-                        .WithRequiredDependent()
-                        .Map( x => x.MapKey( "ProductionOrderRefId" ) );
-
-            //ProductionOrder - Machine
-            modelBuilder.Entity<Machine>()
-                        .HasMany( x => x.ProductionOrders )
-                        .WithOptional()
-                        .Map( x => x.MapKey( "MachineRefId" ) );
-
-            //ProductionOrder - Article
-            modelBuilder.Entity<Article>()
-                        .HasMany( x => x.ProductionOrders )
-                        .WithOptional()
-                        .Map( x => x.MapKey( "ArticleRefId" ) );*/
-            
         }
     }
 }
