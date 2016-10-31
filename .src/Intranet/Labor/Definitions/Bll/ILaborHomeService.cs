3ad412bb4410c176ce0c287b71f8543a5cff21f7
Frank Jordi <frank-jordi@hotmail.com>
2016-10-31 09:53:04 +0100

    add LaborHomeService Interface

---
+++ .src/Intranet/Labor/Definitions/Bll/ILaborHomeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intranet.Labor.Definition.Bll
+{
+    /// <summary>
+    ///     The Interface for the LaborHomeService
+    /// </summary>
+    interface ILaborHomeService
+    {
+        /// <summary>
+        ///     Gets the LaborHomeViewModel
+        /// </summary>
+        /// <returns>The LaborHomeViewModel</returns>
+        LaborHomeViewModel GetLaborHomeViewModel();
+    }
+}
