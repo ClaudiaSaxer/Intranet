09d25eca68b439422d1ff8334790d3ba49b3df23
Saxer Claudia <csaxer@hsr.ch>
2016-11-01 10:04:00 +0100

    model for db od incontinence pad test value

--- .src/Intranet/Labor/Model/labor/IncontinencePadTestValue.cs
+++ .src/Intranet/Labor/Model/labor/IncontinencePadTestValue.cs
@@ -1,6 +1,174 @@
-﻿namespace Intranet.Labor.Model.labor
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intranet.Labor.Model.labor
 {
+    /// <summary>
+    ///     Class representing incontinence pad test value
+    /// </summary>
     public class IncontinencePadTestValue
     {
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the id of the incontinence pad test value
+        /// </summary>
+        /// <value>the incontinence pad test value id </value>
+        [Key]
+        [DatabaseGenerated( DatabaseGeneratedOption.Identity )]
+        public Int32 IncontinencePadTestValueId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the time the  incontinence pad was created
+        /// </summary>
+        /// <value>the time the  incontinence pad was created</value>
+        public TimeSpan IncontinencePadTime { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the month the incontinence pad expires
+        /// </summary>
+        /// <value>the expire montn</value>
+        public Int32 ExpireMonth { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the year the incontinence pad expires
+        /// </summary>
+        /// <value>the expire year</value>
+        public Int32 ExpireYear { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value for the rewet dry
+        /// </summary>
+        /// <value>the value for the rewet dry</value>
+        public Double RewetFreeDryValue { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the RW for the rewet
+        /// </summary>
+        /// <value>the RW for the rewet</value>
+        public RWType RewetFreeRw { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value for the rewet wet
+        /// </summary>
+        /// <value>the value for the rewet wet</value>
+        public Double RewetFreeWetValue { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the difference of the rewet wet and dry
+        /// </summary>
+        /// <value>the difference of the rewet</value>
+        public Double RewetFreeDifference { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the weight of the retention
+        /// </summary>
+        /// <value>the retention weight</value>
+        public Double RetentionWeight { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the weight of the retention wet
+        /// </summary>
+        /// <value>the weight of the retention wet</value>
+        public Double RetentionWetValue { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value for the rentention after the zentrifuge
+        /// </summary>
+        /// <value>the retention value after zentrifuge</value>
+        public Double RetentionAfterZentrifuge { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the retention absorbation value
+        /// </summary>
+        /// <value>the retention absorbation value</value>
+        public Double RetentionAbsorbtion { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the retention end value
+        /// </summary>
+        /// <value>the retention end value</value>
+        public Double RetentionEndValue { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the RW for the retention
+        /// </summary>
+        /// <value>the RW for the retention</value>
+        public RWType RetentionRw { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the weight of the pad for the acquisition
+        /// </summary>
+        /// <value>the weight of the acquisition</value>
+        public Double AcquisitionWeight { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the acquisition after the first addition
+        /// </summary>
+        /// <value>the acquisition after the first addition</value>
+        public Double AcquisitionTimeFirst { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the acquisition after the second addition
+        /// </summary>
+        /// <value>the acquisition after the second addition</value>
+        public Double AcquisitionTimeSecond { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the acquisition after the third addition
+        /// </summary>
+        /// <value>the acquisition after the third addition</value>
+        public Double AcquisitionTimeThird { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the acquisition RW after the first addition
+        /// </summary>
+        /// <value>the acquisition RW after the first addition</value>
+        public RWType AcquisitionTimeFirstRw { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the acquisition RW after the second addition
+        /// </summary>
+        /// <value>the acquisition  RW after the second addition</value>
+        public RWType AcquisitionTimeSecondRw { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the acquisition  RW after the third addition
+        /// </summary>
+        /// <value>the acquisition RW after the third addition</value>
+        public RWType AcquisitionTimeThirdRw { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the weight of a wet pad for the rewet after the acquisition time
+        /// </summary>
+        /// <value>the weight of a wet pad for the rewet after the acquisition time</value>
+        public Double RewetAfterAcquisitionTimeWetWeight { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the weight of a dry pad for the rewet after the acquisition time
+        /// </summary>
+        /// <value>the weight of a dry pad for the rewet after the acquisition time</value>
+        public Double RewetAfterAcquisitionTimeDryWeight { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the weight difference of a  pad for the rewet after the acquisition time
+        /// </summary>
+        /// <value>the weight difference of a  pad for the rewet after the acquisition time</value>
+        public Double RewetAfterAcquisitionTimeWeightDifference { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the rw of a pad for the rewet after the acquisition time
+        /// </summary>
+        /// <value>the rw of a pad for the rewet after the acquisition time</value>
+        public RWType RewetAfterAcquisitionTimeRw { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the test sheet of theincontinence pad test value
+        /// </summary>
+        /// <value>gets or sets the test sheet</value>
+        public TestSheet TestSheet { get; set; }
+
+        #endregion
     }
 }
