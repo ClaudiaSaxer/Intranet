82e6e1d1754f95d5ce9bbda4a80242ae98b7e83d
Saxer Claudia <csaxer@hsr.ch>
2016-11-01 15:20:19 +0100

    viewmodels for creator

--- .src/Intranet/Labor/ViewModel/PenetrationTimeViewModel.cs
+++ .src/Intranet/Labor/ViewModel/PenetrationTimeViewModel.cs
@@ -1,9 +1,46 @@
-﻿namespace Intranet.Labor.ViewModel
+﻿using System;
+
+namespace Intranet.Labor.ViewModel
 {
     /// <summary>
     /// Class representing the view model of the penetration time
     /// </summary>
     public class PenetrationTimeViewModel
     {
+
+        /// <summary>
+        ///     Gets or sets the production code from the diaper
+        /// </summary>
+        /// <value>the production code from the diaper</value>
+        public String ProductionCode { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the weight the diaper has if it is dry
+        /// </summary>
+        /// <value>the weight of the dry diaper</value>
+        public Double WeightyDiaperDry { get; set; }
+        /// <summary>
+        ///     Gets or sets the value of the penetrationTime of the first addition
+        /// </summary>
+        /// <value>penetration time value for the first addition</value>
+        public Double PenetrationTimeAdditionFirst { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value of the penetrationTime of the second addition
+        /// </summary>
+        /// <value>penetration time value for the second addition</value>
+        public Double PenetrationTimeAdditionSecond { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value of the penetrationTime of the third addition
+        /// </summary>
+        /// <value>penetration time value for the third addition</value>
+        public Double PenetrationTimeAdditionThird { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value of the penetrationTime of the fourth addition
+        /// </summary>
+        /// <value>penetration time value for the fourth addition</value>
+        public Double PenetrationTimeAdditionFourth { get; set; }
     }
 }
--- .src/Intranet/Labor/ViewModel/RetentionViewModel.cs
+++ .src/Intranet/Labor/ViewModel/RetentionViewModel.cs
@@ -1,9 +1,47 @@
-﻿namespace Intranet.Labor.ViewModel
+﻿using System;
+using Intranet.Labor.Model.labor;
+
+namespace Intranet.Labor.ViewModel
 {
     /// <summary>
     /// Class representing the view model for the retention
     /// </summary>
     public class RetentionViewModel
     {
+        /// <summary>
+        ///     Gets or sets the production code from the diaper
+        /// </summary>
+        /// <value>the production code from the diaper</value>
+        public String ProductionCode { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the weight the diaper has if it is dry
+        /// </summary>
+        /// <value>the weight of the dry diaper</value>
+        public Double WeightyDiaperDry { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the RW of the retention
+        /// </summary>
+        /// <value>the RW of the retention</value>
+        public RwType RetentionRw { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the rentention value after the zentrifuge
+        /// </summary>
+        /// <value>the retention value after zentrifuge</value>
+        public Double RetentionAfterZentrifugeValue { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the rentention percent after the zentrifuge
+        /// </summary>
+        /// <value>the retention percent after the zentrifuge</value>
+        public Double RetentionAfterZentrifugePercent { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value for the SAP g/Höwi
+        /// </summary>
+        /// <value>the SAP g/Höwi value</value>
+        public Double SapGHoewiValue { get; set; }
     }
 }
