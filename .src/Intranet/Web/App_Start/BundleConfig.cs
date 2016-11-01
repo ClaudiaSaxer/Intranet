82f5cd7857cd84edbb35df562ff19c14e823cecf
Saxer Claudia <csaxer@hsr.ch>
2016-11-01 17:04:05 +0100

    add bootstrap js and complete css to bundle

--- .src/Intranet/Web/App_Start/BundleConfig.cs
+++ .src/Intranet/Web/App_Start/BundleConfig.cs
@@ -15,17 +15,21 @@ namespace Intranet.Web
         public static void RegisterBundles( BundleCollection bundles )
         {
             bundles.Add( new ScriptBundle( "~/bundles/jquery" ).Include(
-                             "~/resources/js/jquery-{version}.js" ) );
+                             "~/resources/js/jquery-{version}.js",
+                             "~/resources/js/bootstrap.min.js" ) );
 
             bundles.Add( new ScriptBundle( "~/bundles/jqueryval" ).Include(
-                             "~/resources/js/jquery.validate*") );
+                             "~/resources/js/jquery.validate*" ) );
 
             bundles.Add( new ScriptBundle( "~/bundles/bootstrap" ).Include(
                              "~/resources/js/bootstrap.js",
-                             "~/resources/js/respond.js") );
-            bundles.Add( new StyleBundle("~/bundles/css").Include(
-                             "~/resources/css/intigena/bootstrap.css",
-                             "~/resources/css/intigena/site.css") );
+                             "~/resources/js/respond.js" ) );
+
+            bundles.Add( new StyleBundle( "~/bundles/css" ).Include(
+                             "~/resources/css/bootstrap.min.css",
+                             "~/resources/css/bootstrap-theme.min.css",
+                             "~/resources/css/bootstrap-toggle.min.css",
+                             "~/resources/css/intigena/site.css" ) );
         }
     }
 }
