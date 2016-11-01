0692cf8dab03ba8a7714c740e6e1bc6abd57b134
Saxer Claudia <csaxer@hsr.ch>
2016-11-01 10:31:01 +0100

    add new dbset

--- .src/Intranet/Labor/Dal/LaborContext.cs
+++ .src/Intranet/Labor/Dal/LaborContext.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.Entity;
 using Intranet.Labor.Model;
+using Intranet.Labor.Model.labor;
 
 namespace Intranet.Labor.Dal
 {
@@ -8,7 +9,26 @@ namespace Intranet.Labor.Dal
     /// </summary>
     public class LaborContext : DbContext
     {
-        #region Properties
+        /// <summary>
+        ///     This method is called when the model for a derived context has been initialized, but
+        ///     before the model has been locked down and used to initialize the context.  The default
+        ///     implementation of this method does nothing, but it can be overridden in a derived class
+        ///     such that the model can be further configured before it is locked down.
+        /// </summary>
+        /// <remarks>
+        ///     Typically, this method is called only once when the first instance of a derived context
+        ///     is created.  The model for that context is then cached and is for all further instances of
+        ///     the context in the app domain.  This caching can be disabled by setting the ModelCaching
+        ///     property on the given ModelBuidler, but note that this can seriously degrade performance.
+        ///     More control over caching is provided through use of the DbModelBuilder and DbContextFactory
+        ///     classes directly.
+        /// </remarks>
+        /// <param name="modelBuilder"> The builder that defines the model for the context being created. </param>
+        protected override void OnModelCreating( DbModelBuilder modelBuilder )
+        {
+        }
+
+        #region Intigena 
 
         /// <summary>
         ///     Gets or sets the articles for the labor
@@ -50,23 +70,54 @@ namespace Intranet.Labor.Dal
 
         #endregion
 
+        #region Instron 
+
         /// <summary>
-        ///     This method is called when the model for a derived context has been initialized, but
-        ///     before the model has been locked down and used to initialize the context.  The default
-        ///     implementation of this method does nothing, but it can be overridden in a derived class
-        ///     such that the model can be further configured before it is locked down.
+        ///     Gets or sets the instrons
         /// </summary>
-        /// <remarks>
-        ///     Typically, this method is called only once when the first instance of a derived context
-        ///     is created.  The model for that context is then cached and is for all further instances of
-        ///     the context in the app domain.  This caching can be disabled by setting the ModelCaching
-        ///     property on the given ModelBuidler, but note that this can seriously degrade performance.
-        ///     More control over caching is provided through use of the DbModelBuilder and DbContextFactory
-        ///     classes directly.
-        /// </remarks>
-        /// <param name="modelBuilder"> The builder that defines the model for the context being created. </param>
-        protected override void OnModelCreating( DbModelBuilder modelBuilder )
-        {
-        }
+        /// <value>the instrons</value>
+        public DbSet<Instron> Instrons { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the instron values
+        /// </summary>
+        /// <value>the instron values</value>
+        public DbSet<InstronValue> InstronValues { get; set; }
+
+        #endregion
+
+        #region Labor 
+
+        /// <summary>
+        ///     Gets or sets the baby diaper test values
+        /// </summary>
+        /// <value>the baby diaper test values</value>
+        public DbSet<BabyDiaperTestValue> BabyDiaperTestValues { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the incontinence pad test values
+        /// </summary>
+        /// <value>the incontinence pad test values</value>
+        public DbSet<IncontinencePadTestValue> IncontinencePadTestValues { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the test values
+        /// </summary>
+        /// <value>the test values</value>
+        public DbSet<TestValue> TestValues { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the test notes
+        /// </summary>
+        /// <value>the test notes</value>
+        public DbSet<TestNote> TestNotes { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the test sheets
+        /// </summary>
+        /// <value>the test sheets</value>
+        public DbSet<TestSheet> TestSheets { get; set; }
+
+        #endregion
     }
 }
