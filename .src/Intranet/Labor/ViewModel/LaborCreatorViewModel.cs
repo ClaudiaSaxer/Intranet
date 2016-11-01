cb10a1481a1377ac152de6021deedf7bd4b05881
Saxer Claudia <csaxer@hsr.ch>
2016-11-01 11:34:02 +0100

    lobor creator view model

--- .src/Intranet/Labor/ViewModel/LaborCreatorViewModel.cs
+++ .src/Intranet/Labor/ViewModel/LaborCreatorViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace Intranet.Labor.ViewModel
+﻿using Intranet.Labor.Model.labor;
+
+namespace Intranet.Labor.ViewModel
 {
     /// <summary>
     ///     Class representing the view model for the labor creator
     /// </summary>
     public class LaborCreatorViewModel
     {
-      
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets a test sheet for the creator viewmodel
+        /// </summary>
+        /// <value>a test sheet</value>
+        public TestSheet TestSheet { get; set; }
+
+        #endregion
     }
 }
