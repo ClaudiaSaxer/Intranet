6d8cc5dd1eafabd433b4ffe271260714b2a2e72d
Saxer Claudia <csaxer@hsr.ch>
2016-11-02 09:06:45 +0100

    model test

---
+++ .src/Intranet.Testing/Model/ModuleTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Intranet.Model.Test
+{
+    /// <summary>
+    /// Class representing Module Test
+    /// </summary>
+    public class ModuleTest
+    {
+        /// <summary>
+        ///     Sets and gets all Properties
+        /// </summary>
+        [Fact]
+        public void SetPropertiesTest()
+        {
+
+            var module = new Module()
+            {
+                Visible = true,Name = "Heidi",Roles = new List<Role>(),Type = ModuleType.Main,ModuleId = 666,ControllerName = "thecontroller",ActionName = "theaction",Submodules = new List<Module>(),AreaName = "thearea",Description = "blabla blub"
+            };
+            module.Should()
+                  .NotBeNull( "is initialized" );
+            module.Name.Should()
+                  .Be( "Heidi" );
+            module.Roles.Should()
+                  .NotBeNull( "is initialized" );
+            module.ActionName.Should()
+                  .Be( "theaction" );
+            module.AreaName.Should()
+                  .Be( "thearea" );
+            module.ControllerName.Should()
+                  .Be( "thecontroller" );
+            module.Description.Should()
+                  .Be( "blabla blub" );
+            module.Submodules.Should()
+                  .NotBeNull( "is initialized" );
+            module.Visible.Should()
+                  .BeTrue( "is set true" );
+            module.Type.Should()
+                  .Be( ModuleType.Main );
+            module.ModuleId.Should()
+                  .Be( 666 );
+
+        }
+    }
+}
---
+++ .src/Intranet.Testing/Model/ModuleTypeTest.cs
@@ -0,0 +1,36 @@
+﻿using Extend;
+using FluentAssertions;
+using Xunit;
+
+namespace Intranet.Model.Test
+{
+    /// <summary>
+    ///     Class representing Module Type Test
+    /// </summary>
+    public class ModuleTypeTest
+    {
+        /// <summary>
+        ///     Test Main Type
+        /// </summary>
+        [Fact]
+        public void MainTypeTest()
+            => ModuleType.Main.ToInt32().Should()
+                         .Be( 0 );
+
+        /// <summary>
+        ///     Test Setting Type
+        /// </summary>
+        [Fact]
+        public void SettingTypeTest()
+            => ModuleType.Setting.ToInt32().Should()
+                         .Be( 2 );
+
+        /// <summary>
+        ///     Test Subt Type
+        /// </summary>
+        [Fact]
+        public void SubTypeTest()
+            => ModuleType.Sub.ToInt32().Should()
+                         .Be( 1 );
+    }
+}
