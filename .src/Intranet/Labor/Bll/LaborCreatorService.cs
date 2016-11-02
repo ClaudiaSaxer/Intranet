3f271472bfbd9d417e29a785081b037d4be46076
Saxer Claudia <csaxer@hsr.ch>
2016-11-02 16:49:13 +0100

    update model

--- .src/Intranet/Labor/Bll/LaborCreatorService.cs
+++ .src/Intranet/Labor/Bll/LaborCreatorService.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Extend;
 using Intranet.Common;
+using Intranet.Labor.Bll;
+using Intranet.Labor.Model;
+using Intranet.Labor.Model.labor;
 using Intranet.Labor.ViewModel;
 
 namespace Intranet.Web.Areas.Labor.Controllers
@@ -42,25 +47,44 @@ namespace Intranet.Web.Areas.Labor.Controllers
         /// <returns>the labor creator view model</returns>
         public LaborCreatorViewModel GetLaborCreatorViewModel( Int32 testSheetId )
         {
-            var vm = InstanceCreator
-                .CreateInstanceOptions<LaborCreatorViewModel>()
-                .WithFactory( x => new List<String>( RandomValueEx.GetRandomStrings( 10 ) ) )
-                .For( x => x.IsTypeOf<ICollection<String>>() )
-                .Complete()
-                .CreateInstance();
+            var testSheet = LaborCreatorBll.getTestSheetForId( testSheetId );
+            var babydiaper = testSheet.TestValues.ToList()
+                                      .Where( x => x.ArticleTestType== ArticleType.BabyDiaper  );
+            var rewets = ToRewetTestValuesCollection( babydiaper );
 
-            //TODO
-            /*    var vm = new LaborCreatorViewModel
-                {
-                    Producer = "Intigena",
-                    Shift =  ShiftType.Morning.ToFriendlyString(),
-                    FaNr = "Fa123",
-                    ProductName = "Babydream",
-                    SizeName = "Maxi-Plus",
-                    CreatedDate= "12.12.2015"
-                   
-                };*/
+            var vm = new LaborCreatorViewModel
+            {
+                Producer = "Intigena",
+                Shift = testSheet.ShiftType.ToFriendlyString(),
+                FaNr = testSheet.FaNr,
+                ProductName = testSheet.ProductName,
+                SizeName = testSheet.SizeName,
+                CreatedDate = testSheet.CreatedDateTime.ToShortDateString(),
+                Rewets = rewets
+            };
             return vm;
+        }
+
+        private ICollection<RewetTestValue> ToRewetTestValuesCollection( IEnumerable<TestValue> testValue )
+        {
+            var rewets = testValue.ToList()
+                                  .Where( x => x.TestValueType == TestValueType.Single && (x.BabyDiaperTestValue.TestType == TestTypeBabyDiaper.Rewet  ||  x.BabyDiaperTestValue.TestType==TestTypeBabyDiaper.PenetrationTime)).ForEach( x => ToRewetTestValue( x.BabyDiaperTestValue, x.LastEditedPerson) );
+            
+        }
+
+        private RewetTestValue ToRewetTestValue(BabyDiaperTestValue rewet,String testPerson )
+        {
+            var vm = new RewetTestValue
+            {
+                TestInfo = new TestInfo
+                {
+                    TestPerson = testPerson,
+                    ProductionCode = rewet.
+                    WeightyDiaperDry = rewet
+                },
+                Rewet = new Rewet()
+            }
+            return null;
         }
 
         #endregion
