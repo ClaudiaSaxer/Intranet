4d77559066561a3b9f1ade60e147658a7f86f09e
Saxer Claudia <csaxer@hsr.ch>
2016-11-02 09:08:54 +0100

    Init viewmodel tests

---
+++ .src/Intranet.Testing/ViewModel/HomeViewModelTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intranet.ViewModel.Test
+{
+    /// <summary>
+    /// Class representing Tests for HomeViewModel
+    /// </summary>
+    public class HomeViewModelTest
+    {
+    }
+}
