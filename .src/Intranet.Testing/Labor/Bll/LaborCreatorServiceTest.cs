47dc44651fc2d63cfbabbcf27f90d417e933a421
Saxer Claudia <csaxer@hsr.ch>
2016-11-03 11:11:32 +0100

    create test for laborcreator service

---
+++ .src/Intranet.Testing/Labor/Bll/LaborCreatorServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intranet.Web.Areas.Labor.Controllers;
+using Xunit;
+
+namespace Intranet.Labor.Bll.Test
+{
+    /// <summary>
+    /// Class representing the Test for the class <see cref="LaborCreatorService"/>
+    /// </summary>
+   public class LaborCreatorServiceTest
+    {
+     
+    }
+}
