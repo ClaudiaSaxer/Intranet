a17472915d28356687a86cf2d43fad15a78b3353
Saxer Claudia <csaxer@hsr.ch>
2016-11-03 17:32:58 +0100

    ShiftTypeExtension Test

---
+++ .src/Intranet.Testing/Labor/Bll/ShiftTypeExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Intranet.Labor.Model;
+using Xunit;
+
+namespace Intranet.Labor.Bll.Test
+{
+    /// <summary>
+    ///     Class representing Test for ShiftTypeExtension
+    /// </summary>
+    public class ShiftTypeExtensionTest
+    {
+        /// <summary>
+        ///     Test ToFriendlyString
+        /// </summary>
+        [Fact]
+        public void ToFriendlyStringTest()
+        {
+            ShiftType.Night.ToFriendlyString()
+                     .Should()
+                     .Be( "Nacht" );
+
+            ShiftType.Late.ToFriendlyString()
+                     .Should()
+                     .Be( "Spät" );
+
+            ShiftType.Morning.ToFriendlyString()
+                     .Should()
+                     .Be( "Morgen" );
+
+        }
+    }
+}
+
