53f6745df87602915d04926e7dc4777049fd9328
Saxer Claudia <csaxer@hsr.ch>
2016-11-03 11:09:02 +0100

    comments

--- .src/Intranet/Labor/Bll/LaborCreatorService.cs
+++ .src/Intranet/Labor/Bll/LaborCreatorService.cs
@@ -84,7 +84,15 @@ namespace Intranet.Web.Areas.Labor.Controllers
             return vm;
         }
 
-        private TestValue TestTestValueOnlyExactlyOneHasToExist( ICollection<TestValue> testValue, String testType, String valueType )
+        /// <summary>
+        /// Validates the test value where only one item is allowed to exists for the given input parameter. Throws a <see cref="InvalidDataException"/> if more than one or none is found.
+        /// Used for example as average or standard deviation.
+        /// </summary>
+        /// <param name="testValue">the testvalue collection to validate.</param>
+        /// <param name="testType">the testtype that is validate</param>
+        /// <param name="valueType">the valuetype that is validated</param>
+        /// <returns></returns>
+        private TestValue ValidateTestValueOnlyExactlyOneHasToExist( ICollection<TestValue> testValue, String testType, String valueType )
         {
             if ( testValue.Count > 1 )
             {
@@ -101,34 +109,76 @@ namespace Intranet.Web.Areas.Labor.Controllers
             return item;
         }
 
+        /// <summary>
+        /// Gets the BabyDiaperTestValue out of a list of testvalues for the correct <see cref="TestTypeBabyDiaper"/> and <see cref="TestValueType"/>
+        /// </summary>
+        /// <param name="testValues">the test values containing the wanted item</param>
+        /// <param name="testTypeBabyDiaper">the type of the baby diaper. <see cref="TestTypeBabyDiaper"/></param>
+        /// <param name="testValueType">the value type of the baby diaper. <see cref="TestValueType"/></param>
+        /// <returns></returns>
         private BabyDiaperTestValue GetBabyDiaperTestValueForType( IEnumerable<TestValue> testValues, TestTypeBabyDiaper testTypeBabyDiaper, TestValueType testValueType )
         {
             var values = testValues.ToList()
                                    .Where(
                                        x => ( x.TestValueType == testValueType ) && ( x.BabyDiaperTestValue.TestType == testTypeBabyDiaper ) )
                                    .ToList();
-            var item = TestTestValueOnlyExactlyOneHasToExist( values, testTypeBabyDiaper.ToString(), testValueType.ToString() );
+            var item = ValidateTestValueOnlyExactlyOneHasToExist( values, testTypeBabyDiaper.ToString(), testValueType.ToString() );
             return item.BabyDiaperTestValue;
         }
-
+        /// <summary>
+        /// Creates a retention Average with the data from the test values
+        /// </summary>
+        /// <param name="testValues">testvalues containing a item representing data for the average</param>
+        /// <returns>a class representing the retention with the average</returns>
         private Rewet ToRewetAverage( IEnumerable<TestValue> testValues )
             => ToRewet( GetBabyDiaperTestValueForType( testValues, TestTypeBabyDiaper.Rewet, TestValueType.Average ) );
-
+        /// <summary>
+        /// Creates a retention Average with the data from the test values
+        /// </summary>
+        /// <param name="testValues">testvalues containing a item representing data for the average</param>
+        /// <returns>a class representing the retention with the average</returns>
         private Retention ToRetentionAverage( IEnumerable<TestValue> testValues )
             => ToRetention( GetBabyDiaperTestValueForType( testValues, TestTypeBabyDiaper.Retention, TestValueType.Average ) );
 
+        /// <summary>
+        /// Creates a PenetrationTime Average with the data from the test values
+        /// </summary>
+        /// <param name="testValues">testvalues containing a item representing data for the average</param>
+        /// <returns>a class representing the penetration time with the average</returns>
         private PenetrationTime ToPenetrationTimeAverage( IEnumerable<TestValue> testValues )
             => ToPenetrationTime( GetBabyDiaperTestValueForType( testValues, TestTypeBabyDiaper.PenetrationTime, TestValueType.Average ) );
-
+        /// <summary>
+        /// Creates a Rewet Standard Deviation with the data from the test values
+        /// </summary>
+        /// <param name="testValues">testvalues containing a item representing data for the standard deviation</param>
+        /// <returns>a class representing the rewet with the standard deviation</returns>
         private Rewet ToRewetStandardDeviation( IEnumerable<TestValue> testValues )
             => ToRewet( GetBabyDiaperTestValueForType( testValues, TestTypeBabyDiaper.Rewet, TestValueType.StandardDeviation ) );
-
+        /// <summary>
+        /// Creates a Retention Standard Deviation with the data from the test values
+        /// </summary>
+        /// <param name="testValues">testvalues containing a item representing data for the standard deviation</param>
+        /// <returns>a class representing the retention with the standard deviation</returns>
         private Retention ToRetentionStandardDeviation( IEnumerable<TestValue> testValues )
             => ToRetention( GetBabyDiaperTestValueForType( testValues, TestTypeBabyDiaper.Retention, TestValueType.StandardDeviation ) );
 
+        /// <summary>
+        /// Creates a PenetrationTime Standard Deviation with the data from the test values
+        /// </summary>
+        /// <param name="testValues">testvalues containing a item representing data for the standard deviation</param>
+        /// <returns>a class representing the penetration time with the standard deviation</returns>
         private PenetrationTime ToPenetrationTimeStandardDeviation( IEnumerable<TestValue> testValues )
             => ToPenetrationTime( GetBabyDiaperTestValueForType( testValues, TestTypeBabyDiaper.PenetrationTime, TestValueType.StandardDeviation ) );
 
+        /// <summary>
+        /// Creates a collection of a TestValue Type and selects only the needed items from a Collection with help of the input parameter
+        /// </summary>
+        /// <typeparam name="T">the collection type. For example: <see cref="RewetTestValue"/>, <see cref="PenetrationTimeTestValue"/>,<see cref="RetentionTestValue"/></typeparam>
+        /// <param name="testValue">the testvalues that contain the data</param>
+        /// <param name="testValueType">the value type of the test. <see cref="TestValueType"/></param>
+        /// <param name="testTypeBabyDiaper">the type of the baby diaper. <see cref="TestTypeBabyDiaper"/></param>
+        /// <param name="toTestTypeTestValueAction">the action the get the correct data for a collection item. For example: <see cref="ToRewetTestValue"/>, <see cref="ToRetentionTestValue"/>, <see cref="ToPenetrationTimeTestValue"/></param>
+        /// <returns>A Collection with the Type of the output from the <paramref name="toTestTypeTestValueAction"/> given as a input action </returns>
         private Collection<T> ToTestValuesCollectionByTestType<T>( IEnumerable<TestValue> testValue,
                                                                    TestValueType testValueType,
                                                                    ICollection<TestTypeBabyDiaper> testTypeBabyDiaper,
@@ -151,24 +201,45 @@ namespace Intranet.Web.Areas.Labor.Controllers
             return tests;
         }
 
+        /// <summary>
+        /// Creates the rewet test value collection for all singe tests
+        /// </summary>
+        /// <param name="testValues">the testvalues containing the rewet tests</param>
+        /// <returns>a collection for all rewet tests</returns>
         private ICollection<RewetTestValue> ToRewetTestValuesCollection( IEnumerable<TestValue> testValues )
             => ToTestValuesCollectionByTestType( testValues,
                                                  TestValueType.Single,
                                                  new List<TestTypeBabyDiaper> { TestTypeBabyDiaper.Rewet, TestTypeBabyDiaper.PenetrationTime },
                                                  ToRewetTestValue );
 
+        /// <summary>
+        /// Creates the retention  test value collection for all singe tests
+        /// </summary>
+        /// <param name="testValues">the testvalues containing the retention tests</param>
+        /// <returns>a collection for all retention tests</returns>
         private ICollection<RetentionTestValue> ToRetentionTestValuesCollection( IEnumerable<TestValue> testValues )
             => ToTestValuesCollectionByTestType( testValues,
                                                  TestValueType.Single,
                                                  new List<TestTypeBabyDiaper> { TestTypeBabyDiaper.Retention },
                                                  ToRetentionTestValue );
-
+        /// <summary>
+        /// Creates the penetration time test value collection for all singe tests
+        /// </summary>
+        /// <param name="testValues">the testvalues containing the penetration time tests</param>
+        /// <returns>a collection for all penetration time tests</returns>
         private ICollection<PenetrationTimeTestValue> ToPenetrationTimeTestValuesCollection( IEnumerable<TestValue> testValues )
             => ToTestValuesCollectionByTestType( testValues,
                                                  TestValueType.Single,
                                                  new List<TestTypeBabyDiaper> { TestTypeBabyDiaper.PenetrationTime },
                                                  ToPenetrationTimeTestValue );
 
+        /// <summary>
+        /// Creates to TestInfo from diffrent input data
+        /// </summary>
+        /// <param name="testPerson">the person who did the test</param>
+        /// <param name="prodCode">the prodcution code from the diaper</param>
+        /// <param name="weightDiaperDry">the weight of the dry diaper</param>
+        /// <returns></returns>
         private TestInfo toTestInfo( String testPerson, String prodCode, Double weightDiaperDry )
             => new TestInfo
             {
@@ -177,6 +248,13 @@ namespace Intranet.Web.Areas.Labor.Controllers
                 WeightyDiaperDry = weightDiaperDry
             };
 
+        /// <summary>
+        /// Creates the Rewet TestValue from diffrent input data
+        /// </summary>
+        /// <param name="rewet">the baby diaper test value containing the rewet data</param>
+        /// <param name="testPerson">the person who did the test</param>
+        /// <param name="prodCode">the diaper production code</param>
+        /// <returns></returns>
         private RewetTestValue ToRewetTestValue( BabyDiaperTestValue rewet, String testPerson, String prodCode )
         {
             var vm = new RewetTestValue
@@ -186,7 +264,13 @@ namespace Intranet.Web.Areas.Labor.Controllers
             };
             return vm;
         }
-
+        /// <summary>
+        /// Creates the PenetrationTime TestValue from diffrent input data
+        /// </summary>
+        /// <param name="penetrationTime">the baby diaper test value containing the penetration time data</param>
+        /// <param name="testPerson">the person who did the test</param>
+        /// <param name="prodCode">the diaper production code</param>
+        /// <returns></returns>
         private PenetrationTimeTestValue ToPenetrationTimeTestValue( BabyDiaperTestValue penetrationTime, String testPerson, String prodCode )
         {
             var vm = new PenetrationTimeTestValue
@@ -197,6 +281,13 @@ namespace Intranet.Web.Areas.Labor.Controllers
             return vm;
         }
 
+        /// <summary>
+        /// Creates the RetentionTestValues from diffrent input data
+        /// </summary>
+        /// <param name="retention">the baby diaper test value containing the retention data</param>
+        /// <param name="testPerson">the person who did the test</param>
+        /// <param name="prodCode">the diaper production code</param>
+        /// <returns></returns>
         private RetentionTestValue ToRetentionTestValue( BabyDiaperTestValue retention, String testPerson, String prodCode )
         {
             var vm = new RetentionTestValue
