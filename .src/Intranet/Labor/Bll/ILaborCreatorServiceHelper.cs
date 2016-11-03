7b223793b83fe3d3380694b01c5962c3377db57b
Saxer Claudia <csaxer@hsr.ch>
2016-11-03 11:22:07 +0100

    create helper for service and interface

---
+++ .src/Intranet/Labor/Bll/ILaborCreatorServiceHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Intranet.Web.Areas.Labor.Controllers
+{
+    /// <summary>
+    /// Class representing a interface for <see cref="LaborCreatorServiceHelper"/>
+    /// </summary>
+    public interface ILaborCreatorServiceHelper
+    {
+
+    }
+}
