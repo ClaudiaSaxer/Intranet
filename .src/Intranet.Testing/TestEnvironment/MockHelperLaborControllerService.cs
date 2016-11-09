186b3e8f747551c3f3ce5b6decd1d76eaa6abfea
Frank Jordi <frank-jordi@hotmail.com>
2016-11-09 13:33:09 +0100

    laborhomecontroller test

---
+++ .src/Intranet.Testing/TestEnvironment/MockHelperLaborControllerService.cs
@@ -0,0 +1,33 @@
+﻿#region Usings
+
+using Intranet.Labor.Definition;
+using Intranet.Labor.ViewModel;
+using Moq;
+
+#endregion
+
+namespace Intranet.TestEnvironment
+{
+    /// <summary>
+    ///     A mock helper class for Labor Controller Services
+    /// </summary>
+    public class MockHelperLaborControllerService
+    {
+        /// <summary>
+        ///     The Mock for the HomeService
+        /// </summary>
+        public static ILaborHomeService GetLaborHomeService( LaborHomeViewModel viewModel )
+        {
+            var mock = new Mock<ILaborHomeService>
+            {
+                Name = "MockHelperService.GetLaborHomeService",
+                DefaultValue = DefaultValue.Mock
+            };
+
+            mock.Setup( laborHomeService => laborHomeService.GetLaborHomeViewModel() )
+                .Returns( viewModel );
+
+            return mock.Object;
+        }
+    }
+}
