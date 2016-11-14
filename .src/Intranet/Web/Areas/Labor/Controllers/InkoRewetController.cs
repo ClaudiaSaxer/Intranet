0967ab25e725d5063c86a8148e4ebd4e34fc7d41
Frank Jordi <frank-jordi@hotmail.com>
2016-11-14 09:53:44 +0100

    create view

---
+++ .src/Intranet/Web/Areas/Labor/Controllers/InkoRewetController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Extend;
+using Intranet.Common;
+using Intranet.Labor.ViewModel;
+
+namespace Intranet.Web.Areas.Labor.Controllers
+{
+    /// <summary>
+    ///     Class representing the BabyDiaperRewetController
+    /// </summary>
+    public class InkoRewetController : BaseController
+    {
+        #region Ctor
+
+        /// <summary>
+        ///     Initialize a new instance of the <see cref="InkoRewetController" /> class.
+        /// </summary>
+        /// <param name="loggerFactory">A <see cref="ILoggerFactory" />.</param>
+        public InkoRewetController(ILoggerFactory loggerFactory)
+            : base( loggerFactory.CreateLogger( typeof(InkoRewetController) ) )
+        {
+            Logger.Trace("Enter Ctor - Exit.");
+        }
+
+        #endregion
+
+        /// <summary>
+        ///     Loads the InkoRewet Edit View with a new Item for the Test-Sheet
+        /// </summary>
+        /// ///
+        /// <param name="id">The Id of the test-sheet which this Test-Data is for</param>
+        /// <returns>The Edit View filled with the viewModel</returns>
+        public ActionResult Create(Int32 id = 0)
+        {
+            if (id.IsNull())
+                return HttpNotFound();
+            var viewModel = new InkoRewetEditViewModel {NoteCodes = new List<ErrorCode>()};
+            if (viewModel.IsNull())
+                return new HttpNotFoundResult("Das TestSheet ist entweder kein Baby Windel Testsheet oder existiert nicht.");
+            return View("Edit",viewModel);
+        }
+
+        /// <summary>
+        ///     adds a new note to the test
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns>The Edit View</returns>
+        [HttpPost]
+        public ActionResult AddNote(InkoRewetEditViewModel viewModel)
+        {
+            if (viewModel.Notes.IsNull())
+                viewModel.Notes = new List<TestNote>();
+            viewModel.Notes.Add(new TestNote());
+            return View("Edit", viewModel);
+        }
+    }
+}
