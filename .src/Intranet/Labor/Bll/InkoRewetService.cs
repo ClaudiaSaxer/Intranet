85c24eccd997e3f52dae27e09dc22ed3c1e02ce5
Frank Jordi <frank-jordi@hotmail.com>
2016-11-14 10:35:11 +0100

    create interface for service helper

--- .src/Intranet/Labor/Bll/InkoRewetService.cs
+++ .src/Intranet/Labor/Bll/InkoRewetService.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿#region Usings
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Extend;
 using Intranet.Common;
 using Intranet.Labor.Definition;
 using Intranet.Labor.Model.labor;
 using Intranet.Labor.ViewModel;
+
+#endregion
 
 namespace Intranet.Labor.Bll
 {
@@ -22,9 +25,12 @@ namespace Intranet.Labor.Bll
         /// </summary>
         public IBabyDiaperBll TestBll { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the test service helper.
+        /// </summary>
+        public IBabyDiaperServiceHelper TestServiceHelper { get; set; }
 
         #endregion
-
 
         #region Ctor
 
@@ -32,10 +38,10 @@ namespace Intranet.Labor.Bll
         ///     Initialize a new instance of the <see cref="ServiceBase" /> class.
         /// </summary>
         /// <param name="loggerFactory">A <see cref="ILoggerFactory" />.</param>
-        public InkoRewetService(ILoggerFactory loggerFactory)
+        public InkoRewetService( ILoggerFactory loggerFactory )
             : base( loggerFactory.CreateLogger( typeof(InkoRewetService) ) )
         {
-            Logger.Trace("Enter Ctor - Exit.");
+            Logger.Trace( "Enter Ctor - Exit." );
         }
 
         #endregion
@@ -49,7 +55,9 @@ namespace Intranet.Labor.Bll
         /// <returns>The deleted testvalue</returns>
         public TestValue Delete( Int32 testValueId )
         {
-            throw new NotImplementedException();
+            var result = TestBll.DeleteTestValue( testValueId );
+            //BabyDiaperRewetServiceHelper.UpdateRewetAverageAndStv(result.TestSheetRefId);
+            return result;
         }
 
         /// <summary>
@@ -59,7 +67,52 @@ namespace Intranet.Labor.Bll
         /// <returns>The InkoRewetEditViewModel</returns>
         public InkoRewetEditViewModel GetInkoRewetEditViewModel( Int32 rewetTestId )
         {
-            throw new NotImplementedException();
+            var testValue = TestBll.GetTestValue( rewetTestId );
+            if ( testValue.IsNull() )
+            {
+                Logger.Error( "TestValue mit id " + rewetTestId + "existiert nicht in DB!" );
+                return null;
+            }
+            var incontinencePadTestValue = testValue.IncontinencePadTestValue;
+            if ( incontinencePadTestValue.IsNull() )
+            {
+                Logger.Error( "IncontinencePadTestValue mit id " + testValue.TestValueId + "existiert nicht in DB!" );
+                return null;
+            }
+            if ( incontinencePadTestValue.TestType != TestTypeIncontinencePad.RewetFree )
+            {
+                Logger.Error( "Requestet test was not an InkoRewet Test. Id " + testValue.TestValueId );
+                return null;
+            }
+            var testSheet = testValue.TestSheet;
+            if ( testSheet.IsNull() )
+            {
+                Logger.Error( "TestBlatt mit id " + testValue.TestSheetRefId + "existiert nicht in DB!" );
+                return null;
+            }
+            var notes = testValue.TestValueNote;
+            var errors = TestBll.GetAllNoteCodes();
+            var errorCodes = errors.Select( error => new ErrorCode { ErrorId = error.ErrorId, Name = error.ErrorCode + " - " + error.Value } )
+                                   .ToList();
+            if ( notes.IsNull() )
+                notes = new List<TestValueNote>();
+            var testNotes = notes.Select( note => new TestNote { Id = note.TestValueNoteId, ErrorCodeId = note.ErrorRefId, Message = note.Message } )
+                                 .ToList();
+
+            var viewModel = new InkoRewetEditViewModel
+            {
+                TestValueId = rewetTestId,
+                TestSheetId = testValue.TestSheetRefId,
+                TestPerson = testValue.LastEditedPerson,
+                ProductionCode = TestServiceHelper.CreateProductionCode( testSheet ),
+                ProductionCodeDay = testValue.DayInYearOfArticleCreation,
+                ProductionCodeTime = incontinencePadTestValue.IncontinencePadTime,
+                FPDry = incontinencePadTestValue.RewetFreeDryValue,
+                FPWet = incontinencePadTestValue.RewetFreeWetValue,
+                Notes = testNotes,
+                NoteCodes = errorCodes
+            };
+            return viewModel;
         }
 
         /// <summary>
@@ -69,7 +122,38 @@ namespace Intranet.Labor.Bll
         /// <returns>The InkoRewetEditViewModel</returns>
         public InkoRewetEditViewModel GetNewInkoRewetEditViewModel( Int32 testSheetId )
         {
-            throw new NotImplementedException();
+            var testSheet = TestBll.GetTestSheetInfo( testSheetId );
+
+            if ( testSheet.IsNull() )
+            {
+                Logger.Error( "TestBlatt mit id " + testSheetId + "existiert nicht in DB!" );
+                return null;
+            }
+
+            var errors = TestBll.GetAllNoteCodes();
+            var errorCodes = errors.Select( error => new ErrorCode { ErrorId = error.ErrorId, Name = error.ErrorCode + " - " + error.Value } )
+                                   .ToList();
+
+            var viewModel = new InkoRewetEditViewModel
+            {
+                TestSheetId = testSheetId,
+                TestValueId = -1,
+                ProductionCode = TestServiceHelper.CreateProductionCode( testSheet ),
+                NoteCodes = errorCodes,
+                Notes = new List<TestNote>()
+            };
+
+            var oldTestValue = testSheet.TestValues.Where( t => t.TestValueType == TestValueType.Single )
+                                        .ToList()
+                                        .LastOrDefault();
+            if ( oldTestValue != null )
+            {
+                viewModel.TestPerson = oldTestValue.LastEditedPerson;
+                viewModel.ProductionCodeDay = oldTestValue.DayInYearOfArticleCreation;
+                viewModel.ProductionCodeTime = oldTestValue.IncontinencePadTestValue.IncontinencePadTime;
+            }
+
+            return viewModel;
         }
 
         /// <summary>
@@ -79,7 +163,20 @@ namespace Intranet.Labor.Bll
         /// <returns>The saved or updated TestValue</returns>
         public TestValue Save( InkoRewetEditViewModel viewModel )
         {
-            throw new NotImplementedException();
+            TestValue testValue = null;
+            try
+            {
+                /*testValue = viewModel.TestValueId <= 0
+                    ? BabyDiaperRewetServiceHelper.SaveNewRewetTest(viewModel)
+                    : BabyDiaperRewetServiceHelper.UpdateRewetTest(viewModel);
+                var testSheet = BabyDiaperRewetServiceHelper.UpdateRewetAverageAndStv(viewModel.TestSheetId);*/
+            }
+            catch ( Exception e )
+            {
+                Logger.Error( "Update oder Create new Test Value ist fehlgeschlagen: " + e.Message );
+                testValue = null;
+            }
+            return testValue;
         }
 
         #endregion
---
+++ .src/Intranet/Labor/Definitions/Bll/IInkoRewetServiceHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intranet.Labor.Model.labor;
+using Intranet.Labor.ViewModel;
+
+namespace Intranet.Labor.Definition.Bll
+{
+    /// <summary>
+    ///     The Interface for the incontinence rewet service helper
+    /// </summary>
+    public interface IInkoRewetServiceHelper
+    {
+        /// <summary>
+        ///     Creates an new TestValue from the view model
+        /// </summary>
+        /// <param name="viewModel">the data from the view</param>
+        /// <returns>The created test value</returns>
+        TestValue SaveNewRewetTest(InkoRewetEditViewModel viewModel);
+
+        /// <summary>
+        ///     Updates the Average and standard deviation values of the testsheet for retention values
+        /// </summary>
+        /// <param name="testSheetId">id of the test sheet</param>
+        /// <returns>the updated test sheet</returns>
+        TestSheet UpdateRewetAverageAndStv(Int32 testSheetId);
+
+        /// <summary>
+        ///     Updates an given Testvalue from the view model
+        /// </summary>
+        /// <param name="viewModel">the data from the view</param>
+        /// <returns>the updated test value</returns>
+        TestValue UpdateRewetTest(InkoRewetEditViewModel viewModel);
+    }
+}
