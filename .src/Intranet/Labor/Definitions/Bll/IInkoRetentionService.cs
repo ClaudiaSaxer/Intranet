95cf3d3f3553154b8519e39d36d4b6a1ff1f4a17
Frank Jordi <frank-jordi@hotmail.com>
2016-11-15 10:25:58 +0100

    create interface for inko retention service helper

--- .src/Intranet/Labor/Definitions/Bll/IInkoRetentionService.cs
+++ .src/Intranet/Labor/Definitions/Bll/IInkoRetentionService.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Usings
+
+using System;
+using Intranet.Labor.Model.labor;
+using Intranet.Labor.ViewModel;
+
+#endregion
 
 namespace Intranet.Labor.Definition.Bll
 {
-    interface IInkoRetentionService
+    /// <summary>
+    ///     The Interface for the InkoRetentionService
+    /// </summary>
+    public interface IInkoRetentionService
     {
+        /// <summary>
+        ///     deletes the testvalue
+        /// </summary>
+        /// <param name="testValueId">id of the testvalue</param>
+        /// <returns>The deleted testvalue</returns>
+        TestValue Delete( Int32 testValueId );
+
+        /// <summary>
+        ///     Gets a new InkoRetentionEditViewModel
+        /// </summary>
+        /// <param name="rewetTestId">The Id of the Inko rewet test which will be edited</param>
+        /// <returns>The InkoRewetEditViewModel</returns>
+        InkoRetentionEditViewModel GetInkoRetentionEditViewModel( Int32 rewetTestId );
+
+        /// <summary>
+        ///     Gets the InkoRetentionEditViewModel for edit
+        /// </summary>
+        /// <param name="testSheetId">The Id of the test sheet where the Inko rewet test is for</param>
+        /// <returns>The InkoRewetEditViewModel</returns>
+        InkoRetentionEditViewModel GetNewInkoRetentionEditViewModel( Int32 testSheetId );
+
+        /// <summary>
+        ///     Saves or updates the InkoRetentionEditViewModel
+        /// </summary>
+        /// <param name="viewModel">The viewmodel which will be saved or updated</param>
+        /// <returns>The saved or updated TestValue</returns>
+        TestValue Save( InkoRetentionEditViewModel viewModel );
     }
 }
