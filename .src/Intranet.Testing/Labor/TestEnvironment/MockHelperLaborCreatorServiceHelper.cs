963d0d9ac9f6631a803411dc83f90f6033336ca2
Saxer Claudia <csaxer@hsr.ch>
2016-11-15 17:13:04 +0100

    Mock

---
+++ .src/Intranet.Testing/Labor/TestEnvironment/MockHelperLaborCreatorServiceHelper.cs
@@ -0,0 +1,47 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core.Internal;
+using Intranet.Common;
+using Intranet.Labor.Definition;
+using Intranet.Labor.Definition.Bll;
+using Intranet.Labor.Model;
+using Intranet.Labor.Model.labor;
+using Intranet.Model;
+using Intranet.Web.Areas.Labor.Controllers;
+using Moq;
+
+#endregion
+
+namespace Intranet.Labor.TestEnvironment
+{
+    /// <summary>
+    ///     A mock helper class for labor creator service helper
+    /// </summary>
+    public static class MockHelperLaborCreatorServiceHelper
+    {
+        /// <summary>
+        ///     A mock for LaborCreatorServiceHelper
+        /// </summary>
+        /// <param name="prodCode"></param>
+        /// <returns>a moq for laborcreatorbll</returns>
+        public static ILaborCreatorServiceHelper GetLaborCreatorServiceHelper(String prodCode)
+        {
+            var mock = new Mock<ILaborCreatorServiceHelper>
+            {
+                Name = "MockHelper.ILaborCreatorServiceHelper",
+                DefaultValue = DefaultValue.Mock
+            };
+
+            mock.Setup(x => x.GenerateProdCode(It.IsAny<String>(), It.IsAny<Int32>(), It.IsAny<Int32>(), It.IsAny<TimeSpan>()))
+                .Returns(prodCode);
+
+            return mock.Object;
+        }
+
+
+      
+    }
+}
