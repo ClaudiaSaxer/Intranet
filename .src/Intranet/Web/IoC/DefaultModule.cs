92b076e6b15c01666588997d3696a57653715896
Frank Jordi <frank-jordi@hotmail.com>
2016-11-15 10:39:30 +0100

    register inko retention service helper in autofac

--- .src/Intranet/Web/IoC/DefaultModule.cs
+++ .src/Intranet/Web/IoC/DefaultModule.cs
@@ -193,6 +193,11 @@ namespace Intranet.Web.IoC
                    .As<IInkoRetentionService>()
                    .PropertiesAutowired()
                    .InstancePerRequest();
+
+            builder.RegisterType<InkoRetentionServiceHelper>()
+                   .As<IInkoRetentionServiceHelper>()
+                   .PropertiesAutowired()
+                   .InstancePerRequest();
         }
 
         /// <summary>
