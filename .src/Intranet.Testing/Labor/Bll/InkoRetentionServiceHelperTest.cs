679e215b815acd5a229509e8cff15d1971128b35
Frank Jordi <frank-jordi@hotmail.com>
2016-11-15 14:56:35 +0100

    create test class for inko retention service helper

---
+++ .src/Intranet.Testing/Labor/Bll/InkoRetentionServiceHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Intranet.Labor.Bll.Test
+{
+    /// <summary>
+    ///     Class representing Tests for InkoRetentionServiceHelper
+    /// </summary>
+    public class InkoRetentionServiceHelperTest
+    {
+        #region BaseTestData
+
+
+
+        #endregion
+
+        #region SaveNewRetentionTest Tests
+
+
+
+        #endregion
+
+        #region UpdateRetentionTest Tests
+
+
+
+        #endregion
+
+        #region UpdateRetentionAverageAndStv Tests
+
+        
+
+        #endregion
+    }
+}
