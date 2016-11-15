89c2191628dc55824a93c109e19af0b4d7c50499
Frank Jordi <frank-jordi@hotmail.com>
2016-11-15 10:18:03 +0100

    create view model for inko retention test

---
+++ .src/Intranet/Labor/ViewModel/InkoEdit/InkoRetentionEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intranet.Labor.ViewModel.InkoEdit
+{
+    class InkoRetentionEditViewModel
+    {
+    }
+}
