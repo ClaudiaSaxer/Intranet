8cca7018dafa5c5aa5468c1fb90c4a4425859441
Saxer Claudia <csaxer@hsr.ch>
2016-11-16 19:40:31 +0100

    design view

--- .src/Intranet/Labor/Bll/LaborDashboardService.cs
+++ .src/Intranet/Labor/Bll/LaborDashboardService.cs
@@ -1,4 +1,6 @@
-﻿using Intranet.Common;
+﻿using System.Collections.Generic;
+using Intranet.Common;
+using Intranet.Labor.Model.labor;
 using Intranet.Labor.ViewModel;
 
 namespace Intranet.Web.Areas.Labor.Controllers
@@ -39,9 +41,90 @@ namespace Intranet.Web.Areas.Labor.Controllers
         /// <returns>the LaborDashboardViewModel</returns>
         public LaborDashboardViewModel GetLaborDashboardViewModel()
         {
-            return new LaborDashboardViewModel();
-        }
+            //TODO implement and remove test data
+            return new LaborDashboardViewModel
+                {
+                    DashboardItem = new List<DashboardItem>
+                    {
+                        new DashboardItem
+                        {
+                            MachineName = "M10",
+                            ShiftItems =
+                                new List<ShiftItem>
+                                {
+                                    new ShiftItem
+                                    {
+                                        ProductionOrderItems =
+                                            new List<ProductionOrderItem>
+                                            {
+                                                new ProductionOrderItem { HasNotes = true, RwType = RwType.Ok, ProductionOrderName = "FA1234" },
+                                                new ProductionOrderItem { HasNotes = false, RwType = RwType.Worse, ProductionOrderName = "FA45" },
+                                                new ProductionOrderItem { HasNotes = false, RwType = RwType.Better, ProductionOrderName = "FA122" },
+                                                new ProductionOrderItem { HasNotes = true, RwType = RwType.SomethingWorse, ProductionOrderName = "FA666" }
+                                            }
+                                    },
+                                    new ShiftItem
+                                    {
+                                        ProductionOrderItems =
+                                            new List<ProductionOrderItem>
+                                            {
+                                                new ProductionOrderItem { HasNotes = false, RwType = RwType.Ok, ProductionOrderName = "FA1234" },
+                                                new ProductionOrderItem { HasNotes = true, RwType = RwType.Ok, ProductionOrderName = "FA122" }
+                                            }
+                                    },
+                                    new ShiftItem
+                                    {
+                                        ProductionOrderItems =
+                                            new List<ProductionOrderItem>
+                                            {
+                                                new ProductionOrderItem { HasNotes = false, RwType = RwType.Ok, ProductionOrderName = "FA1234" },
+                                                new ProductionOrderItem { HasNotes = true, RwType = RwType.Ok, ProductionOrderName = "FA122" }
+                                            }
+                                    },
+                                    new ShiftItem
+                                    {
+                                        ProductionOrderItems =
+                                            new List<ProductionOrderItem>
+                                            {
+                                                new ProductionOrderItem
+                                                {
+                                                    HasNotes = true,
+                                                    RwType = RwType.Ok,
+                                                    ProductionOrderName = "FA122"
+                                                }
+                                            }
+                                    }
+                                }
+                        },
+                        new DashboardItem
+                        {
+                            MachineName = "M11",
+                            ShiftItems =
+                                new List<ShiftItem>
+                                {
+                                    new ShiftItem { ProductionOrderItems = new List<ProductionOrderItem>() },
+                                    new ShiftItem { ProductionOrderItems = new List<ProductionOrderItem>() },
+                                    new ShiftItem { ProductionOrderItems = new List<ProductionOrderItem>() },
+                                    new ShiftItem { ProductionOrderItems = new List<ProductionOrderItem>() }
+                                }
+                        },
+                        new DashboardItem
+                        {
+                            MachineName = "M45",
+                            ShiftItems =
+                                new List<ShiftItem>
+                                {
+                                    new ShiftItem { ProductionOrderItems = new List<ProductionOrderItem>() },
+                                    new ShiftItem { ProductionOrderItems = new List<ProductionOrderItem>() },
+                                    new ShiftItem { ProductionOrderItems = new List<ProductionOrderItem>() },
+                                    new ShiftItem { ProductionOrderItems = new List<ProductionOrderItem>() }
+                                }
+                        }
+                    }
+                }
+                ;
 
-        #endregion
+            #endregion
+        }
     }
 }
