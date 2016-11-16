d962a737f4ecce876a5e9d0d101b14f68650c145
Saxer Claudia <csaxer@hsr.ch>
2016-11-16 14:48:51 +0100

    create viewmodel for dashboard

---
+++ .src/Intranet/Labor/ViewModel/LaborDashboard/LaborDashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intranet.Labor.ViewModel.LaborDashboard
+{
+    /// <summary>
+    /// Class representing the viewmodel for the labor dashboard
+    /// </summary>
+    public class LaborDashboardViewModel
+    {
+    }
+}
