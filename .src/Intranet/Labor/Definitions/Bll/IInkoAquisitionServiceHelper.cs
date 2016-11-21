ee5ce24cc6ae554761b7b640abc8b6f90dc661f1
Frank Jordi <frank-jordi@hotmail.com>
2016-11-21 08:59:44 +0100

    implement service

--- .src/Intranet/Labor/Definitions/Bll/IInkoAquisitionServiceHelper.cs
+++ .src/Intranet/Labor/Definitions/Bll/IInkoAquisitionServiceHelper.cs
@@ -6,7 +6,7 @@ using Intranet.Labor.ViewModel;
 
 #endregion
 
-namespace Intranet.Labor.Definition.Bll
+namespace Intranet.Labor.Definition
 {
     /// <summary>
     ///     The Interface for the incontinence acquisition service helper
@@ -18,20 +18,20 @@ namespace Intranet.Labor.Definition.Bll
         /// </summary>
         /// <param name="viewModel">the data from the view</param>
         /// <returns>The created test value</returns>
-        TestValue SaveNewRetentionTest( InkoAquisitionEditViewModel viewModel );
+        TestValue SaveNewAquisitionTest( InkoAquisitionEditViewModel viewModel );
 
         /// <summary>
         ///     Updates the Average and standard deviation values of the testsheet for aquisition values
         /// </summary>
         /// <param name="testSheetId">id of the test sheet</param>
         /// <returns>the updated test sheet</returns>
-        TestSheet UpdateRetentionAverageAndStv( Int32 testSheetId );
+        TestSheet UpdateAquisitionAverageAndStv( Int32 testSheetId );
 
         /// <summary>
         ///     Updates an given Testvalue from the view model
         /// </summary>
         /// <param name="viewModel">the data from the view</param>
         /// <returns>the updated test value</returns>
-        TestValue UpdateRetentionTest( InkoAquisitionEditViewModel viewModel );
+        TestValue UpdateAquisitionTest( InkoAquisitionEditViewModel viewModel );
     }
 }
