2e2effa7d9a37cca86fe811b70bb64e698d9336f
Saxer Claudia <csaxer@hsr.ch>
2016-11-21 16:05:37 +0100

    CalcRwTypeTest

---
+++ .src/Intranet.Testing/Labor/Bll/LaborDashboardServiceHelperTest.cs
@@ -0,0 +1,39 @@
+﻿#region Usings
+
+using FluentAssertions;
+using Intranet.Common;
+using Intranet.Labor.Model.labor;
+using Intranet.Web.Areas.Labor.Controllers;
+using Xunit;
+
+#endregion
+
+namespace Intranet.Labor.Bll.Test
+{
+    /// <summary>
+    ///     Test Class for Labor Dashboard Service Helper
+    /// </summary>
+    public class LaborDashboardServiceHelperTest
+    {
+        /// <summary>
+        ///     Calc Rw type test
+        /// </summary>
+        [Fact]
+        public void CalcRwTypeTest()
+        {
+            var target = new LaborDashboardHelper( new NLogLoggerFactory() );
+            target.CalcRwType( RwType.Ok, RwType.Better )
+                  .Should()
+                  .Be( RwType.Better );
+            target.CalcRwType( RwType.Ok, RwType.Worse )
+                  .Should()
+                  .Be( RwType.Worse );
+            target.CalcRwType( RwType.SomethingWorse, RwType.Worse )
+                  .Should()
+                  .Be( RwType.Worse );
+            target.CalcRwType( RwType.Ok, RwType.SomethingWorse )
+                  .Should()
+                  .Be( RwType.SomethingWorse );
+        }
+    }
+}
