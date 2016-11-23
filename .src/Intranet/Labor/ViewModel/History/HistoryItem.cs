a36c4a8245f451470587e1256297b815cdb6bd9b
Frank Jordi <frank-jordi@hotmail.com>
2016-11-23 10:51:41 +0100

    create viewModel

---
+++ .src/Intranet/Labor/ViewModel/History/HistoryItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intranet.Labor.Model;
+
+namespace Intranet.Labor.ViewModel
+{
+    /// <summary>
+    ///     Class representing an history item for the history viewmodel
+    /// </summary>
+    public class HistoryItem
+    {
+        /// <summary>
+        ///     Gets or sets the ID of the Test Sheet
+        /// </summary>
+        /// <value>
+        ///     The ID of the Test Sheet
+        /// </value>
+        public Int32 TestSheetId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the FaNr of the Test Sheet
+        /// </summary>
+        /// <value>
+        ///     The FaNr of the Test Sheet
+        /// </value>
+        public String FaNr { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Shift
+        /// </summary>
+        /// <value>the shift of the test sheet</value>
+        public ShiftType ShiftType { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the number of the machine
+        /// </summary>
+        /// <value>the number of the machine</value>
+        public String MachineNr { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the timestamp of the creation of the test sheet
+        /// </summary>
+        /// <value>the timestamp of the creation</value>
+        public DateTime CreatedDateTime { get; set; }
+    }
+}
---
+++ .src/Intranet/Labor/ViewModel/History/HistoryViewModel.cs
@@ -0,0 +1,33 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Intranet.Labor.ViewModel
+{
+    /// <summary>
+    ///     Class representing the History ViewModel for the HistoryController + View
+    /// </summary>
+    public class HistoryViewModel
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the FA Number
+        /// </summary>
+        /// <value>
+        ///     The FaNr
+        /// </value>
+        public String FaNr { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Sheets
+        ///     Collection contains an HistoryItem
+        /// </summary>
+        public ICollection<HistoryItem> Sheets { get; set; }
+
+        #endregion
+    }
+}
